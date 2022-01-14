4c8cbf20e9eb347cda74277e218391c524c0c6c8
macarena <macarenaaldanagalvez@gmail.com>
2022-01-14 15:26:12 -0300

    doc

--- src/base/controllers/base.controller.ts
+++ src/base/controllers/base.controller.ts
@@ -9,7 +9,14 @@ import {
   Delete,
 } from '@nestjs/common';
 import { IBaseService } from '../services/iBase.service';
-
+/*
+  Controllers are responsible for handling incoming requests and returning responses to the client.
+  A controller's purpose is to receive specific requests for the application. 
+  The routing mechanism controls which controller receives which requests. 
+  Frequently, each controller has more than one route, and different routes can perform different actions.
+  In order to create a basic controller, we use classes and decorators. 
+  Decorators associate classes with required metadata and enable Nest to create a routing map (tie requests to the corresponding controllers).
+*/
 export class BaseController<T> {
   constructor(private readonly _IBaseService: IBaseService<T>) {}
 
