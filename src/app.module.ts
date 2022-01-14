fc30532885289c73fc6ce27f38b0caf9f24cfe5c
macarena <macarenaaldanagalvez@gmail.com>
2022-01-14 17:04:34 -0300

    modulo insumo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ClienteModule } from './modulo-acceso/cliente/cliente.module';
 import { DetalleFacturaModule } from './modulo-pedido/detalle-pedido/detalle-pedido.module';
 import { FacturaModule } from './modulo-pedido/factura/factura.module';
 import { PedidoModule } from './modulo-pedido/pedido/pedido.module';
+import { ArticuloInsumoModule } from './modulo-stock/articulo-insumo/articulo-insumo.module';
 
 @Module({
   imports: [
