59359c9b931a9520c76ee20aebedccbf04126f07
macarena <macarenaaldanagalvez@gmail.com>
2022-01-14 16:44:44 -0300

    controladores

--- src/modulo-pedido/pedido/pedido.controller.ts
+++ src/modulo-pedido/pedido/pedido.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { BaseController } from 'src/base/controllers/base.controller';
+import { Pedido } from './pedido.entity';
+import { PedidoService } from './pedido.service';
 
 @Controller('api/v1/pedido')
-export class PedidoController {}
+export class PedidoController extends BaseController<Pedido> {
+  constructor(private readonly service: PedidoService) {
+    super(service);
+  }
+}
