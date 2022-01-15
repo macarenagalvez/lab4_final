374935b7e52e044d2311b52d8406059dd2534fa0
macarena <macarenaaldanagalvez@gmail.com>
2022-01-15 19:34:17 -0300

    quitar detalles

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteModule } from './modulo-acceso/cliente/cliente.module';
-import { DetalleFacturaModule } from './modulo-pedido/detalle-pedido/detalle-pedido.module';
 import { FacturaModule } from './modulo-pedido/factura/factura.module';
 import { PedidoModule } from './modulo-pedido/pedido/pedido.module';
 import { ArticuloInsumoModule } from './modulo-stock/articulo-insumo/articulo-insumo.module';
-import { ArticuloManufacturadoDetalleModule } from './modulo-stock/articulo-manufacturado-detalle/articulo-manufacturado-detalle.module';
 import { ArticuloManufacturadoModule } from './modulo-stock/articulo-manufacturado/articulo-manufacturado.module';
 import { RubroArticuloModule } from './modulo-stock/rubro-articulo/rubro-articulo.module';
 import { RubroGeneralModule } from './modulo-stock/rubro-general/rubro-general.module';
@@ -14,12 +12,10 @@ import { RubroGeneralModule } from './modulo-stock/rubro-general/rubro-general.m
   imports: [
     TypeOrmModule.forRoot(),
     ClienteModule,
-    DetalleFacturaModule,
     FacturaModule,
     PedidoModule,
     ArticuloInsumoModule,
     ArticuloManufacturadoModule,
-    ArticuloManufacturadoDetalleModule,
     RubroArticuloModule,
     RubroGeneralModule,
   ],
